02b637747e2486c6a5ac2d5c4627ec4c151c94a1
ragul0427 <ragulhp2704@gmail.com>
2023-11-27 18:49:57 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
 
-function App() {
+const App = () => {
+  const [location, setLocation] = useState(null);
+
+  useEffect(() => {
+    const getLocation = () => {
+      if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(
+          async (position) => {
+            const { latitude, longitude } = position.coords;
+            const locationName = await getLocationName(latitude, longitude);
+            setLocation({ latitude, longitude, locationName });
+          },
+          (error) => {
+            console.error('Error getting location:', error.message);
+          }
+        );
+      } else {
+        console.error('Geolocation is not supported by this browser.');
+      }
+    };
+
+    getLocation();
+  }, []); // Empty dependency array means this effect runs once after the initial render
+
+  const getLocationName = async (latitude, longitude) => {
+    try {
+      const response = await fetch(
+        `https://nominatim.openstreetmap.org/reverse?format=json&lat=${latitude}&lon=${longitude}`
+      );
+
+      const data = await response.json();
+      const locationName = data.display_name;
+      return locationName;
+    } catch (error) {
+      console.error('Error getting location name:', error.message);
+      return null;
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <div>
+      {location ? (
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Latitude: {location.latitude}, Longitude: {location.longitude}, Location: {location.locationName}
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      ) : (
+        <p>Fetching location...</p>
+      )}
     </div>
   );
-}
+};
 
 export default App;
